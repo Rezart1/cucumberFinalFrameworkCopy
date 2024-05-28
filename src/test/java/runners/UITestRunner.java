4495d2ccf4d776e7d5b1a8adfef2083797feb108
yollinfo <56563749+yollinfo@users.noreply.github.com>
2024-05-28 16:29:51 -0400

    Update UITestRunner.java
    enabled tag option in runner class

--- src/test/java/runners/UITestRunner.java
+++ src/test/java/runners/UITestRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         "json:target/cucumber.json"},
         features="src/test/resources/ui_features",
         glue="ui_automation",
-        //tags={"@regression"},
+        tags={"@regression"},
         dryRun = false,
         monochrome = true
 )
